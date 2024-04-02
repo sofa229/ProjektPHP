ee975c62bf494521bdf82d930d69692800f5c4b5
sofa229 <115933945+sofa229@users.noreply.github.com>
2024-04-02 20:04:25 +0200

    footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+
+            <a href="home.html" class="sofsfoot"><div class="sofsfoot" style="font-size:2vw">SOF'S</div>
+              <div class="sofsfoot" style="font-size:2vw">sofsofficialbrand@gmail.com</div>
+           
+    	 <footer style="font-size:2vw">© 2023 SOF’S  All rights reserved.</footer>
+             </div>
+<?php
+      add_scripts();
+    ?>
+</body>
+</html>
