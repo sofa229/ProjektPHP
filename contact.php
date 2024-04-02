2f872d575e42455367d5200bdeaa4af78316c0fa
sofa229 <115933945+sofa229@users.noreply.github.com>
2024-04-02 20:43:02 +0200

    contact.php

---
+++ contact.php
@@ -0,0 +1,43 @@
+<?php
+    include_once('partials/header.php');
+?> 
+
+       
+<script>
+	function showSidebar(){
+		const sidebar=document.querySelector('.sidebar')
+		sidebar.style.display ='flex'
+	}
+	function hideSidebar(){
+		const sidebar=document.querySelector('.sidebar')
+		sidebar.style.display ='none'
+	}	
+</script>
+       
+
+       
+       
+        <h1 style="font-size:13vw">Contact</h1>
+       
+        
+        <p style="font-size:3vw"><strong>
+        You can follow US on social media:</strong>
+        </p>
+        
+         <hr new4>  
+   
+<div class="social">
+	<a href="https://www.instagram.com/sofsofficial/"><img src="black ig.png" class="center"style="width:19%;"></a>
+	<a href="https://www.facebook.com/sofiakucerobags"><img src="black fb.png" class="center"style="width:23%;"></a>
+
+</div>
+
+<hr new4> <!--čiara-->
+<?php
+  include_once('partials/footer.php')
+?>  
+        
+</div>
+    </body>
+
+</html>
